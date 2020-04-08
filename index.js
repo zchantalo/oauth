d33befa71cd0c7ca2c315e6273c0199e4ac7fbc2
zchantalo <talocjohn@gmail.com>
2020-04-08 16:29:34 +0800

    PArt1 redo

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ app.set('view engine', 'ejs');
 
 // create home route
 app.get('/', (req, res)=>{
-    res.render('home');
+    const anydata ={
+     description:{
+      title: "This is a sample description",
+      item:5
+	 }
+	}
+    res.render('home',anydata);
 });
 
 // listen to requests
 app.listen(3000, ()=> {
     console.log('app now listening for requests on port 3000');
-});
+}); 
