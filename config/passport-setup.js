d8f25ddd89391b8296f1b135e49e2e42c6c193af
zchantalo <talocjohn@gmail.com>
2020-04-08 18:26:12 +0800

    part3redo

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -12,5 +12,10 @@ passport.use(
         // passport callback function
         console.log('##########################');
         console.log(profile);
+
+        console.log(profile.id)
+        console.log(profile.displayName)
+        console.log(profile.photos[0].value)
+        console.log(profile._json.picture)
     })
 );
